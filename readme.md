fb370abb4be179e4db26ff95c3ee30b72e655c78
princeofverry <verrykurniawan@students.undip.ac.id>
2024-05-02 18:49:11 +0700

    unit test soal 1

---
+++ readme.md
@@ -0,0 +1,15 @@
+## Usage/Examples
+
+```python
+cara menjalankan program:
+
+python soal1.py
+python soal2.py
+python soal3.py
+python soal3-another-solution.py
+```
+
+## Authors
+
+- [@princeofverry](https://www.github.com/princeofverry)
+- source lain: stackoverflow
--- soal1.py
+++ soal1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import unittest
 
 # Fungsi untuk mencari matriks balikan menggunakan NumPy
 
@@ -19,3 +20,23 @@ if inverse_A is not None:
     print(inverse_A)
 else:
     print("Matriks A tidak memiliki balikan (inverse).")
+
+
+# unit test
+class TestInverseMatrix(unittest.TestCase):
+
+    def test_inverse(self):
+        # Tes untuk matriks yang memiliki balikan
+        matrix = np.array([[1, -1, 2], [3, 0, 1], [1, 0, 2]])
+        expected_result = np.array(
+            [[0.0, 0.4, -0.2], [-1.0, 0.0, 1.0], [0.0, -0.2, 0.6]])
+        self.assertTrue(np.allclose(inverse_matrix(matrix), expected_result))
+
+    def test_singular_matrix(self):
+        # Tes untuk matriks yang tidak memiliki balikan
+        matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        self.assertIsNone(inverse_matrix(matrix))
+
+
+if __name__ == '__main__':
+    unittest.main()
